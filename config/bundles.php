f65a122845bd9bc9ad5d03814a600e8a6dda7edf
Thibault Gattolliat <contact@thibaultg.info>
2021-07-16 12:15:44 +0200

    Update Symfony to 5.3 version

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,4 @@ return [
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
