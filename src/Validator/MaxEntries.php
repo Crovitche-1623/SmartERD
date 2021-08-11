94bab4b7e86c9ec8c6e043920a75a5fb99139a48
Thibault G <gattolliat.thibault@gmail.com>
2021-08-11 15:20:52 +0200

    Create a custom constraint to check max entries instead of callback

---
+++ src/Validator/MaxEntries.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+final class MaxEntries extends Constraint
+{
+    public string $message = 'The maximum number of {{ parentName }} ({{ max }}) for this {{ childName }} has been reached.';
+
+    public function __construct(
+        public int $max,
+        array $groups = null,
+        mixed $payload = null
+    )
+    {
+        $options['max'] = $this->max;
+        parent::__construct($options, $groups, $payload);
+    }
+
+    public function getRequiredOptions(): array
+    {
+        return ['max'];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function validatedBy(): string
+    {
+        return MaxEntriesValidator::class;
+    }
+}
---
+++ src/Validator/MaxEntriesValidator.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Validator;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\NoResultException;
+use ReflectionClass;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+final class MaxEntriesValidator extends ConstraintValidator
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {}
+
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof MaxEntries) {
+            throw new UnexpectedTypeException($constraint, MaxEntries::class);
+        }
+
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) to take care of that
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        // In case the constraint is used on wrong property.
+        if (!is_object($value)) {
+            throw new UnexpectedTypeException($value, 'object');
+        }
+
+        $property = $this->context->getPropertyName();
+
+        $parentFqdn = get_class($this->context->getObject());
+
+        $query = $this->entityManager->createQuery(/** @lang DQL */"
+            SELECT COUNT(c0.id) FROM $parentFqdn c0 WHERE c0.$property = :id
+        ");
+
+        $query->setParameter('id', $value->getId());
+
+        $childShortName = (new ReflectionClass(get_class($value)))->getShortName();
+
+        try {
+            $entriesCount = (int) $query->getSingleScalarResult();
+
+            if ($entriesCount >= $constraint->max) {
+                $this->context->buildViolation($constraint->message)
+                    ->setParameter('{{ max }}', (string) $constraint->max)
+                    ->setParameter('{{ childName }}', $childShortName)
+                    ->setParameter('{{ parentName }}', (new ReflectionClass($parentFqdn))->getShortName())
+                    ->addViolation();
+            }
+        } catch (NoResultException) {
+            $this->context->buildViolation('The %childName% is probably not defined.', [
+                   '%childName%' => $childShortName
+                ])
+                ->addViolation();
+        }
+    }
+}
