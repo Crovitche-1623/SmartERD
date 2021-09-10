455a1a38e2081df34b28313bdfa3f4897a8ea0fa
Crovitche-1623 <contact@thibaultg.info>
2021-09-10 15:58:16 +0200

    feat(ProjectInput): Add the correct files to make it work

---
+++ src/DataTransformer/ProjectInputDataTransformer.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataTransformer;
+
+use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\Dto\ProjectInput;
+use App\Repository\UserRepository;
+use App\Entity\{Project, User};
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
+
+final class ProjectInputDataTransformer implements DataTransformerInterface
+{
+    public function __construct(
+        private Security $security,
+        private UserRepository $userRepository
+    )
+    {}
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param  ProjectInput  $object
+     */
+    public function transform($object, string $to, array $context = []): Project
+    {
+        // It's the user payload from the jwt. The user is not fully hydrated
+        // yet.
+        /** @var  User|null  $userPayload */
+        $userPayload = $this->security->getUser();
+
+        // This exception should not occur if the route is correctly configured
+        // or under a firewall
+        if (!$userPayload) {
+            throw new AuthenticationException(
+                'Authentication required',
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        // Check if the user still exist in database and get the real object to
+        // persist the object correctly
+        $currentUser = $this->userRepository->findOneBy([
+            'username' => $userPayload->getUsername()
+        ]);
+
+        return (new Project)
+            ->setName($object->name)
+            ->setUser($currentUser)
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supportsTransformation(
+        $data,
+        string $to,
+        array $context = []
+    ): bool
+    {
+        // if it's a project we transformed the data already
+        if ($data instanceof Project) {
+            return false;
+        }
+
+        // Check if the book is the desired target and if the input is
+        // configured
+        return Project::class === $to &&
+               null !== ($context['input']['class'] ?? null);
+    }
+}
